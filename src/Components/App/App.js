f76d9c971a319348a68db9ff570169991f59031e
[lindsayepetersen] <lindsayepetersen@gmail.com>
2023-06-27 19:14:00 -0600

    added searchbar

---
+++ src/Components/App/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+// import logo from './logo.svg';
+import { SearchBar } from "../SearchBar";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          {/*  Add a SearchBar component  */}
+          <SearchBar />
+
+          <div className="App-playlist">
+            {/*  Add a SearchResults component  */}
+            {/*  Add a Playlist component  */}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
